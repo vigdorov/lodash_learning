3b6c1a11308e2930c6edcbf150808e4b11e261d2
vigdorov <vigdorov@yandex-team.ru>
2019-11-18 16:48:23 +0300

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
